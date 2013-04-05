3966ed70431fdcc234c2882d6b592cfc752eeb50
James Bergstra <james.bergstra@gmail.com>
2013-04-05 09:41:55 -0400

    remove logpy draft code

--- theano_workspace/logpy_opt.py
+++ theano_workspace/logpy_opt.py
@@ -72,59 +72,13 @@ def shape_dim(shape_of):
         try:
             return int(get_scalar_constant_value(shape_of[x][i]))
         except NotScalarConstantError:
+            pass
+        try:
+            return shape_of[x][i].eval()
+        except:
             return -1 # an unsatisfiable shape
     return shape_dim_i
 
-
-if 0:
-  class LogpyTensorVar(Var, tensor.TensorVariable):
-
-    def _logpy_reify_dispatch(self, s):
-        return s[self]
-
-    def assoc(self, d, other):
-        if 0:
-            if self.type.dtype:
-                if self.type.dtype != other.type.dtype:
-                    return False
-            if self.type.ndim:
-                if self.type.ndim != other.type.ndim:
-                    return False
-        d = d.copy()
-        d[self] = other
-        return d
-
-if 0:
-  class LogpyNumpyInteger(int, Var):
-    # -- int has to go first, before Var so that __new__ doesn't complain,
-    # but then these methods from Var have to be duplicated here...
-    # maybe logpy Var could be built from one class that implements __new__
-    # and a mix-in that provides these?
-    def __str__(self):
-        return "~" + str(self.token)
-    __repr__ = __str__
-
-    def __eq__(self, other):
-        return type(self) == type(other) and self.token == other.token
-
-    def __hash__(self):
-        return hash((type(self), self.token))
-
-
-def lptensor(token, dtype, broadcastable):
-    rval = LogpyTensorVar(token)
-    rval.type = tensor.TensorType(dtype=dtype, broadcastable=broadcastable)
-    rval.name = token
-    return rval
-
-def lpint(val, token):
-    rval = LogpyNumpyInteger(val)
-    rval.token = token
-    return rval
-    #rtype = tensor.TensorType(dtype=dtype, broadcastable=broadcastable)
-    #_var_data = var(token + '_data')
-    #print fakedata
-    #return LogpyTensorConstant(rtype, fakedata, token)
 
 def goalifyN(func):
     funco = goalify(func)
@@ -135,25 +89,39 @@ def goalifyN(func):
             (funco, tmp, result))
     return goalo
 
+def getattrrec(x, *attrs):
+    if attrs:
+        return getattrrec(getattr(x, attrs[0]), *attrs[1:])
+    else:
+        return x
+
+getattro = goalifyN(getattr)
+getattrreco = goalifyN(getattrrec)
+
+def isinstanceo(a, b):
+    def deco(s):
+        if isinstance(a, b):
+            yield s
+    return deco
+
+
 def logpy_optimization(f):
     def deco(node):
         rval, goals = f(node) #-- XXX shape features require node
         matches = run(1, rval, *goals)
         return matches[0] if matches else None
-    deco.__name__ == f.__name__
+    deco.__name__ = f.__name__
     return deco
 
 @register_specialize
 @register_canonicalize
 @local_optimizer()
 @logpy_optimization
-def logpy_cut_whole_incsubtensor(node):
+def logpy_cut_whole_setsubtensor(node):
     # TODO: how to design re-usable patterns? (dtype, ndim, etc.)
     shape_dimo = goalifyN(
         shape_dim(node.fgraph.shape_feature.shape_of))
     #jj = lpint(238908925034, 'j') # -- a number that cannot occur in the graph
-    #x = lptensor('x', 'float32', [False])
-    #y = lptensor('y', 'float32', [False])
     x = tensor.vector()
     y = tensor.vector()
     jj = 12345
